0527f278bee19ab06e640adbd4f127bded9d9fee
MaximizeAy <ayomide.daniel.adeosun@gmail.com>
2025-06-15 22:30:42 +0100

    Mid Snack

--- js/main.js
+++ js/main.js
@@ -1,9 +1,3 @@
-// function toggleReadMore(button) {
-//     const blog = document.getElementById("blogBox");
-//     blog.classList.toggle("expanded");
-//     button.textContent = blog.classList.contains("expanded") ? "Read Less" : "Read More";
-//   }
-
 function toggleBlog(btn, id) {
 const content = document.getElementById(id);
 content.classList.toggle('expanded');
